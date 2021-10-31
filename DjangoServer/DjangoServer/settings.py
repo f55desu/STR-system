a0879975ca6915a7a9f112f327781f441055a0b8
f55desu <f55dota217@gmail.com>
2021-10-31 22:01:33 +0300

    Added static files png.
    Co-authored-by: Vladimir Nikulin <JoelWillLiveForever@users.noreply.github.com>

--- DjangoServer/DjangoServer/settings.py
+++ DjangoServer/DjangoServer/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# MEDIA_ROOT = './media'
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = [
