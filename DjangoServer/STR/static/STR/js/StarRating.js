91af2e4a53ed943f003bc1464c1bd519c20d876b
f55desu <f55dota217@gmail.com>
2021-11-10 22:21:21 +0300

    Fixed stars rating overall
    Co-authored-by: TheEZIC <TheEZIC@users.noreply.github.com>

--- DjangoServer/STR/static/STR/js/StarRating.js
+++ DjangoServer/STR/static/STR/js/StarRating.js
@@ -1,82 +1,79 @@
+const MAX_VALUE = 5;
 
+function initRatings(ratingsContainerSelector) {
+    const ratingsContainer = document.querySelector(ratingsContainerSelector);
+    const ratings = Array.from(ratingsContainer.querySelectorAll(".rating"));
 
-document.addEventListener("DOMContentLoaded", () => {
-    const ratings = document.querySelectorAll('.rating');
+    ratings.forEach(r => initRating(r, ratings));
+}
 
-    if(ratings.length > 0) {
-        initRatings();
-    }
+function initRating(rating, ratings) {
+    initRatingVars(rating);
 
-    function initRatings() {
-        let ratingActive, ratingValue;
-        const MAX_VALUE = 5;
+    const isReadonly = rating.hasAttribute("readonly");
 
-        for (let i = 0; i < ratings.length; i++) {
-            const rating = ratings[i];
-            initRating(rating, i == ratings.length - 1);
-        }
+    setRatingActiveWidth();
+    setAVGRating(ratings);
+    setRating(rating, isReadonly, ratings);
+}
 
-        function initRating(rating, readonly = false) {
-            initRatingVars(rating);
+function initRatingVars(rating) {
+    ratingActive = rating.querySelector('.rating__active');
+    ratingValue = rating.querySelector('.rating__value');
+}
 
-            setRatingActiveWidth();
-            setAVGRating();
-            setRating(rating, readonly);
-        }
+function setRatingActiveWidth(index = ratingValue.innerHTML) {
+    const ratingActiveWidth = index / MAX_VALUE * 100;
+    ratingActive.style.width = ratingActiveWidth + "%";
+}
 
-        function initRatingVars(rating) {
-            ratingActive = rating.querySelector('.rating__active');
-            ratingValue = rating.querySelector('.rating__value');
-        }
+function setAVGRating(ratings) {
+    let values = [];
 
-        function setRatingActiveWidth(index = ratingValue.innerHTML) {
-            const ratingActiveWidth = index / MAX_VALUE * 100;
-            ratingActive.style.width = ratingActiveWidth + "%";
-        }
+    ratings.forEach((r, i) => {
+        const rating = r.querySelector('.rating__value');
 
-        function setAVGRating() {
-            let values = [];
+        if (r.hasAttribute("readonly") || !r.hasAttribute("value")) 
+            return;
 
-            ratings.forEach((r, i) => {
-                const rating = r.querySelector('.rating__value');
-                if (i === ratings.length - 1) return;
-                values.push(Number(rating.innerHTML));
-            })
+        values.push(Number(rating.innerHTML));
+    })
 
-            const lastRating = ratings[ratings.length - 1];
+    const lastRatings = Array.from(ratings)
+        .filter(r => r.hasAttribute("readonly"))
+        .forEach(r => {
             const avg = values.reduce((a, b) => a + b, 0) / values.length;
-            console.log(lastRating)
-            lastRating.querySelector(".rating__value").innerHTML = avg;
-            lastRating.querySelector(".rating__value").value = avg;
-            lastRating.querySelector(".rating__active").style.width = `${avg / MAX_VALUE * 100}%`;
-        }
-        
-        function setRating(rating, readonly) {
-            const ratingItems = rating.querySelectorAll('.rating__item');
 
-            for (let i = 0; i < ratingItems.length; i++) {
-                const ratingItem = ratingItems[i];
+            r.querySelector(".rating__value").innerHTML = avg;
+            r.querySelector(".rating__value").value = avg;
+            r.querySelector(".rating__active").style.width = `${avg / MAX_VALUE * 100}%`;
+        }) 
+}
 
-                if (!readonly) {
-                    ratingItem.addEventListener("mouseenter", e => {
-                        initRatingVars(rating);
-                        setRatingActiveWidth(ratingItem.value);
-                    });
-    
-                    ratingItem.addEventListener("mouseleave", e => {
-                        setRatingActiveWidth(ratingValue.value);
-                    });
-    
-                    ratingItem.addEventListener("click", e => {
-                        initRatingVars(rating);
-    
-                        ratingValue.innerHTML = i + 1;
-                        ratingValue.value = i  + 1;
-                        setRatingActiveWidth();
-                        setAVGRating();
-                    });
-                }
-            }
+function setRating(rating, readonly, ratings) {
+    const ratingItems = rating.querySelectorAll('.rating__item');
+
+    for (let i = 0; i < ratingItems.length; i++) {
+        const ratingItem = ratingItems[i];
+
+        if (!readonly) {
+            ratingItem.addEventListener("mouseenter", e => {
+                initRatingVars(rating);
+                setRatingActiveWidth(ratingItem.value);
+            });
+
+            ratingItem.addEventListener("mouseleave", e => {
+                setRatingActiveWidth(ratingValue.value);
+            });
+
+            ratingItem.addEventListener("click", e => {
+                initRatingVars(rating);
+
+                ratingValue.innerHTML = i + 1;
+                ratingValue.value = i  + 1;
+                setRatingActiveWidth();
+                setAVGRating(ratings);
+            });
         }
     }
-})
+}
